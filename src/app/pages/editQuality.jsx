c88ee0ed90c089f81c41c84f7ccb329999dea6e1
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-11-11 00:21:08 +0300

    qualities_fast-company__catching unexpected errors globaly__4

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -3,25 +3,32 @@ import EditForm from "../components/ui/editForm";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
+axios.interceptors.response.use(
+    (response) => response,
+    function (error) {
+        const expectedErrors =
+            error.response &&
+            error.response.status >= 400 &&
+            error.response.status < 500;
+        if (!expectedErrors) {
+            console.log("Unexpected Error");
+        }
+        return Promise.reject(error);
+    }
+);
+
 const EditQualityPage = () => {
     const [quality, setQuality] = useState(null);
     const id = useParams().id;
     const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`;
+
     const handleSubmit = async (data) => {
         try {
             await axios
                 .put(qualityEndPoint, data)
                 .then((response) => console.log(response.data.content));
         } catch (error) {
-            const expectedErrors =
-                error.response &&
-                error.response.status >= 400 &&
-                error.response.status < 500;
-            if (!expectedErrors) {
-                console.log("Unexpected Error");
-            } else {
-                console.log("Expected Error");
-            }
+            console.log("Expected Error");
         }
     };
     useEffect(async () => {
