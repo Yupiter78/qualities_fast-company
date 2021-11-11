452485db8f0e9778c38c68b1707a2a12e73b4440
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-11-12 00:09:32 +0300

    qualities_fast-company__displaying errors with toastify__8

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import logger from "./log.service";
+import { toast } from "react-toastify";
 
 axios.interceptors.response.use(
     (response) => response,
@@ -10,7 +11,7 @@ axios.interceptors.response.use(
             error.response.status < 500;
         if (!expectedErrors) {
             logger.log(error);
-            console.log("Unexpected Error");
+            toast.error("Something was wrong. Try it later");
         }
         return Promise.reject(error);
     }
