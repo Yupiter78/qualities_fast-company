7ce3954273894cc0789a928fcbeb660ad67103bf
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-11-14 17:34:49 +0300

    qualities_fast-company__redirect__17

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import QualityForm from "../components/ui/qualityForm";
 import { useQualities } from "../hooks/useQualities";
 
 const EditQualityPage = () => {
     const id = useParams().id;
+    const history = useHistory();
     const { getQuality, updateQuality } = useQualities();
     const quality = getQuality(id);
     console.log("quality:", quality);
 
     const handleSubmit = (data) => {
-        updateQuality(data);
+        updateQuality(data).then((data) => {
+            if (data) history.push("/");
+        });
     };
 
     return (
